9259c89231b480f4a113d534fa7b094dcb9386f7
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-15 16:11:13 +0530

    Add files via upload

---
+++ p7.py
@@ -0,0 +1,13 @@
+# wapp to read radius from the user
+# print area and circumference
+
+n1 = float(input("enter radius"))
+pi = 3.1459
+area = pi* n1* n1
+circum = 2* pi* n1
+
+print(" area = ",round(area,2))
+print(" circum = ", round(circum,2))
+
+# float() funtion can read decimal numbers and integers both 
+# so using float() functn is always benificial 
